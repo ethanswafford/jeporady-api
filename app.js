cdaf4429cf20aaee282dd436a299fd089c392df4
Ethan Swafford <ethanswafford@yahoo.com>
2022-05-27 03:35:08 -0500

    devPush

--- app.js
+++ app.js
@@ -3,28 +3,28 @@
 let clues = [];
 let usedClues = [];
 
-// build questions/jeporady clues array
-
-fetch('https://jservice.io/api/random?count=4')
-    .then(response => response.json())
-    .then(clues => {
-        for (let clue of clues) {
-            usedClues.push(clue)
-
-            let details = document.createElement("details")
-            let summary = document.createElement("summary")
-
-            details.append(summary)
-            summary.append(`Value = $${clue.value}| Category: ${clue.category.title}; ${clue.question}`)
-            details.append(clue.answer)
-            document.body.append(details)
-        }
-        console.log(usedClues)
-    })
-
 // notes from dmg arrow func demo
 // want to use arrow functions for my event listeners 
 
-document.addEventListener("keydown", () => {
+document.addEventListener("click", () => {
+    let input = document.getElementById("input")
 
+    // build questions/jeporady clues array
+
+    fetch('https://jservice.io/api/random?count=4')
+        .then(response => response.json())
+        .then(clues => {
+            for (let clue of clues) {
+                usedClues.push(clue)
+
+                let details = document.createElement("details")
+                let summary = document.createElement("summary")
+
+                details.append(summary)
+                summary.append(`Value = $${clue.value}| Category: ${clue.category.title}; ${clue.question}`)
+                details.append(clue.answer)
+                document.body.append(details)
+            }
+            console.log(usedClues)
+        })
 });
